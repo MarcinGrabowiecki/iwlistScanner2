bec78130fcec19c79cfd471ac3ee87029020e73e
Marcin Grabowiecki <marcin.grabowiecki@gmail.com>
2015-08-14 22:08:39 +0200

    correct versions, specs

--- src/test/scala/samples/specs.scala
+++ src/test/scala/samples/specs.scala
@@ -1,8 +1,9 @@
 package samples
 
 import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
 import org.specs2.mutable._
-import org.specs2.runner._
+//import org.specs2.runner._
   
 
 /**
