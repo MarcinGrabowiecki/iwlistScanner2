3db04974df9328d946f4391cfb1891f492a8f551
Marcin Grabowiecki <marcin.grabowiecki@cgm.com>
2015-08-17 07:29:24 +0200

    qualityMatcher

--- src/main/scala/marcin/Saga.scala
+++ src/main/scala/marcin/Saga.scala
@@ -10,12 +10,12 @@ import java.util.Date
 
 class Saga extends Printer {
 
-
-  private val ie = """\s*IE: (.*)""".r
-  private val unknown = """Unknown: (.*)""".r
-  private val ieee = """(IEEE|WPA) (.*)""".r
-  private val bitrates = """\s*Bit Rates:(.*)""".r
-  private val bitratesCont = """\s{30}([0-9].*)""".r
+  private val rIe = """\s*IE: (.*)""".r
+  private val rUnknown = """Unknown: (.*)""".r
+  private val rIeee = """(IEEE|WPA) (.*)""".r
+  private val rBitrates = """\s*Bit Rates:(.*)""".r
+  private val rBitratesCont = """\s{30}([0-9].*)""".r
+  private val rQuality="""\s{20}Quality=(../..)  Signal level=-(..) dBm.*""".r
 
   @tailrec
   private def process(lr: LogReader, cell: Cell, date: Date): Unit = {
@@ -39,10 +39,6 @@ class Saga extends Printer {
       }
     }
 
-    //    if (line.startsWith(" "*20+"IE: ")) {
-    //      line = line.replace("                    IE: ", "                    IE-")
-    //    }
-
     new Matcher( """::: \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} (\d{10})""")
       .forText(line)
       .forMatching(m => retDate = new Date(m.group(1).toLong * 1000)
@@ -62,25 +58,23 @@ class Saga extends Printer {
 
     if (line.startsWith(" " * 20)) {
 
+
+
       line match {
-        case ie(text) => text match {
-          case unknown(text1) => retCel.addField("Unknown", text1)
-          case ieee(prefix, text) => retCel.addField(prefix, text); suckIndent(prefix)
+        case rIe(text) => text match {
+          case rUnknown(text1) => retCel.addField("Unknown", text1)
+          case rIeee(prefix, text) => retCel.addField(prefix, text); suckIndent(prefix)
           case _ =>
         }
-        case bitrates(values) => retCel.addField("BitRates", values)
-        case bitratesCont(values) => retCel.addField("BitRatesCont", values)
+        case rBitrates(values) => retCel.addField("BitRates", values)
+        case rBitratesCont(values) => retCel.addField("BitRatesCont", values)
+        case rQuality(qual,lvl) => retCel.addField("Quality", qual).addField("SignalLevel", lvl)
         case _ =>
       }
 
       new Matcher( """([A-Z].*):(.*)""")
         .setStartingSpaces(20).forText(line)
         .forMatching(m => retCel.addField(m.group(1), m.group(2))
-        )
-
-      new Matcher( """Quality=(../..)  Signal level=-(..) dBm""")
-        .setStartingSpaces(20).forText(line)
-        .forMatching(m => retCel.addField("Quality", m.group(1)).addField("SignalLevel", m.group(2))
         )
 
     }
