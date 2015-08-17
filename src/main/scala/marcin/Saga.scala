c72a1b90b6a6e8f11e788d86b06309796726f64e
Marcin Grabowiecki <marcin.grabowiecki@cgm.com>
2015-08-17 06:58:48 +0200

    formatting

--- src/main/scala/marcin/Saga.scala
+++ src/main/scala/marcin/Saga.scala
@@ -14,8 +14,8 @@ class Saga extends Printer {
   private val ie = """\s*IE: (.*)""".r
   private val unknown = """Unknown: (.*)""".r
   private val ieee = """(IEEE|WPA) (.*)""".r
-  private val bitrates="""\s*Bit Rates:(.*)""".r
-  private val bitratesCont="""\s{30}([0-9].*)""".r
+  private val bitrates = """\s*Bit Rates:(.*)""".r
+  private val bitratesCont = """\s{30}([0-9].*)""".r
 
   @tailrec
   private def process(lr: LogReader, cell: Cell, date: Date): Unit = {
@@ -25,63 +25,63 @@ class Saga extends Printer {
     var retDate = date
     if (line == null) return
 
-    def suckIndent(pref:String):Unit={
-      line=lr.getLine
-      if(line.startsWith(" "*23)){
-        val tmps=pref+"-"+line.trim
-        val v=tmps.split(": ");
-        var vv="+"
-        if(v.size>1) vv=v(1)
-        cell addField(tmps.split(" ")(0),vv)
+    def suckIndent(pref: String): Unit = {
+      line = lr.getLine
+      if (line.startsWith(" " * 23)) {
+        val tmps = pref + "-" + line.trim
+        val v = tmps.split(": ");
+        var vv = "+"
+        if (v.size > 1) vv = v(1)
+        cell addField(tmps.split(" ")(0), vv)
         suckIndent(pref)
       } else {
         lr.unread
       }
     }
 
-//    if (line.startsWith(" "*20+"IE: ")) {
-//      line = line.replace("                    IE: ", "                    IE-")
-//    }
+    //    if (line.startsWith(" "*20+"IE: ")) {
+    //      line = line.replace("                    IE: ", "                    IE-")
+    //    }
 
     new Matcher( """::: \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} (\d{10})""")
       .forText(line)
       .forMatching(m => retDate = new Date(m.group(1).toLong * 1000)
-    )
+      )
 
     retCel.addField("scanDate", "" + retDate)
 
-    if (line.startsWith(" "*10+"Cell ")) {
+    if (line.startsWith(" " * 10 + "Cell ")) {
       retCel.write
       retCel = new Cell(printer)
       new Matcher( """Cell (..) - Address: (.*)""")
         .setStartingSpaces(21)
         .forText(line)
         .forMatching(m => retCel.addField("Cell", m.group(1)).addField("Address", m.group(2))
-      )
+        )
     }
 
     if (line.startsWith(" " * 20)) {
 
       line match {
-        case ie(text)=>text match {
-          case unknown(text1)=> retCel.addField("Unknown",text1)
-          case ieee(prefix,text)=> retCel.addField(prefix,text); suckIndent(prefix)
+        case ie(text) => text match {
+          case unknown(text1) => retCel.addField("Unknown", text1)
+          case ieee(prefix, text) => retCel.addField(prefix, text); suckIndent(prefix)
           case _ =>
         }
-        case bitrates(values)=>retCel.addField("BitRates",values)
-        case bitratesCont(values)=>retCel.addField("BitRatesCont",values)
+        case bitrates(values) => retCel.addField("BitRates", values)
+        case bitratesCont(values) => retCel.addField("BitRatesCont", values)
         case _ =>
       }
 
       new Matcher( """([A-Z].*):(.*)""")
         .setStartingSpaces(20).forText(line)
         .forMatching(m => retCel.addField(m.group(1), m.group(2))
-      )
+        )
 
       new Matcher( """Quality=(../..)  Signal level=-(..) dBm""")
         .setStartingSpaces(20).forText(line)
         .forMatching(m => retCel.addField("Quality", m.group(1)).addField("SignalLevel", m.group(2))
-      )
+        )
 
     }
     process(lr, retCel, retDate)
